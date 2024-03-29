3cc3b05095d7107e8e31884c2900d72a2e7bace2
datta-nilotpal <163278549+datta-nilotpal@users.noreply.github.com>
2024-03-30 05:21:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,3 +33,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
   activateLinkOnScroll();
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
