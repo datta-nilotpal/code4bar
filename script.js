9ff9ce5b5559026d51f064227f860886f873bd1c
datta-nilotpal <163278549+datta-nilotpal@users.noreply.github.com>
2024-03-30 22:54:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function() {
+  const header = document.querySelector('header');
   const sections = document.querySelectorAll('section[id]');
   const navLinks = document.querySelectorAll('nav a, .topnav a');
 
   function activateLinkOnScroll() {
     let scrollPosition = window.scrollY + window.innerHeight / 2;
 
-    sections.forEach(section => {
+    sections.forEach((section) => {
       const sectionTop = section.getBoundingClientRect().top + window.scrollY;
       const sectionBottom = sectionTop + section.offsetHeight;
       if (scrollPosition >= sectionTop && scrollPosition <= sectionBottom) {
-        navLinks.forEach(link => {
+        navLinks.forEach((link) => {
           link.classList.remove('active');
           if (link.getAttribute('href') === '#' + section.id) {
             link.classList.add('active');
@@ -19,17 +20,27 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  window.addEventListener('scroll', activateLinkOnScroll);
-  activateLinkOnScroll(); // Activate on initial load
-
-  // Adding smooth scroll behavior for anchor links
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
+  window.addEventListener('scroll', function() {
+    let scrollRange = window.innerHeight * 0.1;
+    let scroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let opacity = Math.min(scroll / scrollRange, 1);
+    header.style.backgroundColor = `rgba(4, 1, 18, ${opacity})`;
+    
+    activateLinkOnScroll();
   });
+
+  activateLinkOnScroll();
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+
