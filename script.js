072c906fe38e3a217f1946ac0fa0c9346d7ea80a
datta-nilotpal <163278549+datta-nilotpal@users.noreply.github.com>
2024-03-30 21:55:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
   const header = document.querySelector('header');
   const sections = document.querySelectorAll('section[id]');
   const navLinks = document.querySelectorAll('nav a, .topnav a');
@@ -6,11 +6,11 @@ document.addEventListener("DOMContentLoaded", function() {
   function activateLinkOnScroll() {
     let scrollPosition = window.scrollY + window.innerHeight / 2;
 
-    sections.forEach((section) => {
+    sections.forEach(section => {
       const sectionTop = section.getBoundingClientRect().top + window.scrollY;
       const sectionBottom = sectionTop + section.offsetHeight;
       if (scrollPosition >= sectionTop && scrollPosition <= sectionBottom) {
-        navLinks.forEach((link) => {
+        navLinks.forEach(link => {
           link.classList.remove('active');
           if (link.getAttribute('href') === '#' + section.id) {
             link.classList.add('active');
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 
-  window.addEventListener('scroll', function() {
+  window.addEventListener('scroll', () => {
     let scrollRange = window.innerHeight * 0.3;
     let scroll = document.body.scrollTop || document.documentElement.scrollTop;
     let opacity = Math.min(scroll / scrollRange, 1);
@@ -30,16 +30,15 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   activateLinkOnScroll();
-});
 
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
+  // Adding smooth scroll behavior for anchor links
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
 
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
     });
+  });
 });
