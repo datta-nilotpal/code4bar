b567bef05dc318d457b9353502839f38ba34b351
datta-nilotpal <163278549+datta-nilotpal@users.noreply.github.com>
2024-03-30 14:16:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const header = document.querySelector('header');
   const sections = document.querySelectorAll('section[id]');
   const navLinks = document.querySelectorAll('nav a, .topnav a');
-  let lastScrollPosition = 0; // New variable to store the last scroll position
+  let lastScrollPosition = window.pageYOffset; // Initialize lastScrollPosition at the current scroll position
 
   function activateLinkOnScroll() {
     let scrollPosition = window.scrollY + window.innerHeight / 2;
@@ -22,27 +22,29 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   window.addEventListener('scroll', function() {
+    let currentScrollPosition = window.pageYOffset; // Get the current scroll position
     let scrollRange = window.innerHeight * 0.3; // 30vh
     let scroll = document.body.scrollTop || document.documentElement.scrollTop;
     let opacity = Math.min(scroll / scrollRange, 1);
     header.style.backgroundColor = `rgba(4, 1, 18, ${opacity})`;
+    
     activateLinkOnScroll();
 
-    // Hide/show header on scroll up/down
-    if (scroll > lastScrollPosition) {
-      // Scrolling down, bring the header back
-      header.style.top = '0';
+    // Show/hide header based on the scroll direction
+    if (currentScrollPosition > lastScrollPosition) {
+      // Scrolling down, hide the header
+      header.style.top = '-100px'; // Adjust this value to match your header's height
     } else {
-      // Scrolling up, move the header out of view
-      header.style.top = '-100px'; // Adjust as needed based on your header's height
+      // Scrolling up, show the header
+      header.style.top = '0';
     }
-    lastScrollPosition = scroll <= 0 ? 0 : scroll; // Update lastScrollPosition, but not below 0
+    // Update lastScrollPosition to the current scroll position
+    lastScrollPosition = currentScrollPosition;
   });
 
   activateLinkOnScroll();
 });
 
-// Smooth scroll for anchor links remains unchanged
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
